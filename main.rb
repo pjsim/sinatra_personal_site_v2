6030d3e5d5397af69f3e0579ef5f06898f246da2
Philip Simnonds <phillipsimmonds@linuxmint.linuxmint-domain>
2013-11-18 09:26:18 +1100

    test bootstrap

--- main.rb
+++ main.rb
@@ -1,6 +1,10 @@
 require 'bundler'
 Bundler.require
 
+configure do
+  set :public_folder, Proc.new { File.join(root, "static") }
+end
+
 get '/' do
 	@title = "Phillip Simmonds's Personal Site"
 	slim :index	
@@ -10,4 +14,23 @@ __END__
 ### views ###
 
 @@index
-h1 HI
+ul#myTab.nav.nav-tabs
+	li.active
+    	a href="#home" Home
+  	li
+    	a href="#profile" Profile
+  	li
+    	a href="#messages" Messages
+  	li
+    	a href="#settings" Settings
+.tab-content
+ 	#home.tab-pane.active ...dsads
+  	#profile.tab-pane ...asddsaad
+  	#messages.tab-pane ...adsdsaewq
+  	#settings.tab-pane ...eqwwqe
+
+javascript:
+	| $('#myTab a').click(function (e) {
+	| e.preventDefault();
+	| $(this).tab('show');
+	| })
